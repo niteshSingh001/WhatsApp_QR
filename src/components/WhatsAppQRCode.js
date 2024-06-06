56c550dbd9fb38d574cfaf58ef946af5bfc89a8e
Nitesh Singh <ns368497@gmail.com>
2024-06-06 18:02:50 +0530

    latest commit

--- src/components/WhatsAppQRCode.js
+++ src/components/WhatsAppQRCode.js
@@ -3,7 +3,7 @@ import QRCode from "react-qr-code";
 
 const WhatsAppQRCode = () => {
   // Access the phone number from the environment variable
-  const phoneNumber = 6387876587;
+  const phoneNumber = 18455379661;
 
   // Create the WhatsApp link
   const whatsappLink = `https://wa.me/${phoneNumber}`;
